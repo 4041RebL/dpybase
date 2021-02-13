049f8ebc2dc3757576a850ad014964783f3c19ce
4041RebL <62462942+4041RebL@users.noreply.github.com>
2021-02-13 17:16:35 +0530

    cus that was twice

--- bot.py
+++ bot.py
@@ -7,8 +7,6 @@ bot = commands.Bot(command_prefix = "!", intents = intents)
 
 @bot.event
 async def on_ready():
-	print("The bot is ready!")
-
 	bot.load_extension("on_boot.ext_load")
 
 bot.run(os.environ.get("TOKEN"))
